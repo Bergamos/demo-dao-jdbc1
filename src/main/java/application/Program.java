c59e095fe095fc8b63f63bc50bbdc6706a6242f0
Alisson Bergamin <alisson.bergs@gmail.com>
2023-01-25 05:29:45 -0300

    Department entity class

---
+++ src/main/java/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import entities.Department;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        var obj = new Department(1, "Books");
+        System.out.println(obj);
+
+
+    }
+
+}
